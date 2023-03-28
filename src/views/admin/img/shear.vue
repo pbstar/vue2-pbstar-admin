a87cdf6d9443b492c28f6fb3777eae6cb38a47c0
pengbo <pengbo@hopefound.cn>
2023-03-28 14:56:49 +0800

    11

---
+++ src/views/admin/img/shear.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="box">1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "adminImgShear",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.box {
+  background-color: #fff;
+  margin: 10px;
+  padding: 10px;
+  min-height: calc(100% - 20px);
+}
+</style>
