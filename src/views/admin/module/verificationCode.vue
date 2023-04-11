58d63ce2f7ae9029b1da5dbff3b0f05bbcfd78ce
pengbo <pengbo@hopefound.cn>
2023-04-11 10:52:25 +0800

    验证码

--- src/views/admin/module/verificationCode.vue
+++ src/views/admin/module/verificationCode.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <div class="box">
-      <div class="verificationCodeBox">
-        <verificationCode />
-        <verificationCode
-          type="2"
-          :contentWidth="120"
-          identifyCodes="123456789"
-          @changeCode="changeCode2"
-        />
+      <div class="boxs">
+        <div class="verificationCodeBox">
+          <verificationCode ref="verificationCode1" />
+          <el-button type="text" @click="toUpdate1">看不清</el-button>
+        </div>
+        <p>文字验证码</p>
+      </div>
+      <div class="boxs">
+        <div class="verificationCodeBox">
+          <verificationCode
+            ref="verificationCode2"
+            type="2"
+            :contentWidth="120"
+            identifyCodes="123456789"
+            @changeCode="changeCode2"
+          />
+          <el-button type="text" @click="toUpdate2">看不清</el-button>
+        </div>
+        <p>计算验证码</p>
       </div>
     </div>
   </div>
@@ -30,18 +41,11 @@ export default {
     changeCode2(e) {
       console.log(e);
     },
-    toClear() {
-      this.$refs.signature.toClear();
+    toUpdate1() {
+      this.$refs.verificationCode1.changeCode();
     },
-    toSave() {
-      this.$refs.signature.toSave().then((res) => {
-        if (res != "NotSignned") {
-          this.list.push(res.base64);
-          this.$refs.signature.toClear();
-        } else {
-          this.$message.warning("签名内容为空！");
-        }
-      });
+    toUpdate2() {
+      this.$refs.verificationCode2.changeCode();
     },
   },
 };
@@ -52,7 +56,30 @@ export default {
   margin: 10px;
   padding: 10px;
   min-height: calc(100% - 20px);
-  // .verificationCodeBox {
-  // }
+  display: flex;
+  .boxs {
+    width: 320px;
+    height: 160px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    margin: 20px;
+    p {
+      line-height: 50px;
+      border-top: 1px solid #eee;
+      margin-top: 30px;
+      width: 100%;
+      text-align: center;
+      color: #666;
+    }
+    .verificationCodeBox {
+      display: flex;
+      align-items: flex-end;
+      .el-button {
+        margin-left: 5px;
+      }
+    }
+  }
 }
 </style>
